6c5df4c83684553958429141029f0930d322fb94
Douglas Esteves <douglas.brsoftware@gmail.com>
2016-03-31 21:13:32 -0300

    Create rover-iot-mkr1000.ino

---
+++ rover-iot-mkr1000.ino
@@ -0,0 +1,185 @@
+#include <WiFi101.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiSSLClient.h>
+#include <WiFiUdp.h>
+#include <SPI.h>
+#include <WiFi101.h>
+
+//L298N,
+int IN1 = 3;
+int IN2 = 4;
+int IN3 = 5;
+int IN4 = 6;
+
+char ssid[] = "LHC";      //  your network SSID (name)
+char pass[] = "tijolo22";   // your network password
+int keyIndex = 0;                 // your network key Index number (needed only for WEP)
+int ledpin = 7;
+bool val = true;
+
+int status = WL_IDLE_STATUS;
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(9600);      // initialize serial communication
+  //Define os pinos como saida l298N
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  //Genuino MKR1000
+  Serial.print(" LHC - Rover IoT ");
+  Serial.print("Start Serial ");
+  pinMode(ledpin, OUTPUT);      // set the LED pin mode
+  Serial.print("WiFi101 shield: ");
+  
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("NOT PRESENT");
+    return; // don't continue
+  }
+  Serial.println("DETECTED");
+  
+  // attempt to connect to Wifi network:
+  while ( status != WL_CONNECTED) {
+    digitalWrite(ledpin, LOW);
+    Serial.print("Attempting to connect to Network named: ");
+    Serial.println(ssid);                   // print the network name (SSID);
+    digitalWrite(ledpin, HIGH);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  server.begin();                           // start the web server on port 80
+  printWifiStatus();                        // you're connected now, so print out the status
+  digitalWrite(ledpin, HIGH);
+  
+}
+
+
+void loop() {
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/F\">here</a> GET FRONT <br>");
+            client.print("Click <a href=\"/S\">here</a> GET STOPING <br>");
+            client.print("Click <a href=\"/L\">here</a> GET LEFT <br>");
+            client.print("Click <a href=\"/R\">here</a> GET RIGHT <br>");
+            client.print("Click <a href=\"/B\">here</a> GET BACK <br>");
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          }
+          else {      // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        }
+        else if (c != '\r') {    // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        if (currentLine.endsWith("GET /F")) {
+          front();               // GET frente
+        }
+
+        if (currentLine.endsWith("GET /S")) {
+          stoping();               // GEt stop
+        }
+
+        if (currentLine.endsWith("GET /L")) {
+          left();               // GET left
+        }
+        
+        if (currentLine.endsWith("GET /R")) {
+          right();               // GET right
+        }
+
+        if (currentLine.endsWith("GET /B")) {
+          back();               // GET Back
+        }
+        
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
+}
+
+void  front(){
+   //Gira o Motor A no sentido horario
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+}
+
+void  stoping (){
+    //Gira o Motor A no sentido horario
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, HIGH);
+}
+
+void  left (){
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);    
+}
+
+void  right (){
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);    
+}
+
+void  back (){
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+}
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+  // print where to go in a browser:
+  Serial.print("User yout firefox http://");
+  Serial.print(ip);
+}
